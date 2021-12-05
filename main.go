febb943b5f5119b4ede25f5b24b11de052bf4576
Ben <benknight135@gmail.com>
2021-12-05 16:19:22 +0000

    add gocv to main

--- main.go
+++ main.go
@@ -30,21 +30,12 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func webcam() {
-	webcam, _ := gocv.VideoCaptureDevice(0)
-	window := gocv.NewWindow("Hello")
-	img := gocv.NewMat()
-
-	for {
-		webcam.Read(&img)
-		window.IMShow(img)
-		window.WaitKey(1)
-	}
-}
-
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+
+	img := gocv.IMRead("static/test.png", gocv.IMReadColor)
+	gocv.IMWrite("static/test_save.png", img)
 
 	routerConfig := cors.Config{
 		AllowAllOrigins:  true,
